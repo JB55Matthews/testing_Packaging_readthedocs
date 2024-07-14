ccd9d51d16d10ca99cb78fcf5c3c32614bbd9198
Jason Matthews <168492258+JB55Matthews@users.noreply.github.com>
2024-07-14 19:37:27 -0230

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,14 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = ".rst"
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
