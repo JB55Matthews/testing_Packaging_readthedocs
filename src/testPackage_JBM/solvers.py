043f6f898b5e527683ed1f0d8899263078a13d4f
Jason Matthews <168492258+JB55Matthews@users.noreply.github.com>
2024-07-14 14:17:19 -0230

    Update solvers.py

--- src/testPackage_JBM/solvers.py
+++ src/testPackage_JBM/solvers.py
@@ -2,6 +2,11 @@
 from .pinn.order1pinnClass import ode_solution
 
 def solve_order1ODE_pinn(eqn, order, init_data, t_bdry=[0,1], N_pde=100, epochs=1000):
+    """Solves a first order ode
+        Args:
+        eqn (string):
+            Equation of ODE in string which equals 0. Function u(t).
+    """
 
 
     solObj = ode_solution(eqn, order, init_data, t_bdry, N_pde, epochs)
